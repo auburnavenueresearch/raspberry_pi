1af0ab270b570e3038b3247e779e2c495e3c7ed7
Robert Belozi Lee <belozi@localhost.localdomain>
2015-10-07 21:26:58 -0400

    tweepy is working/ 1st push from fedora 22 laptop

--- python_programs/tweepy_tutorial.py
+++ python_programs/tweepy_tutorial.py
@@ -1,11 +1,22 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+consumer_key = 
+consumer_secret = 
+access_token = 
+access_token_secret = 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
+    
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
-    print tweet.txt
+    print tweet.text
      
+
+try:
+    redirect_url = auth.get_authorization_url()
+
+except tweepy.TweepError:
+    print "Error! Failed to get request token."
